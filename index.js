219e35fdf769f83ad12dc2f2f41f628196691477
ejb <mail@elliotbentley.com>
2017-05-13 10:37:55 -0400

    getEntries function

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
+const Spanner = require('@google-cloud/spanner');
+const spanner = Spanner();
+const instanceId = 'rupaul-9-halftime-entries';
+const databaseId = 'season9halftime';
+
+exports.getEntries = function() {
+    // Gets a reference to a Cloud Spanner instance and database
+    const instance = spanner.instance(instanceId);
+    const database = instance.database(databaseId);
+
+    // The query to execute
+    const query = {
+      sql: 'SELECT * FROM Entries'
+    };
+
+    // Execute the query
+    return database.run(query)
+      .then((results) => {
+        const rows = results[0].map((row) => row.toJSON());
+        rows.forEach((row) => {
+          res.write(JSON.stringify(row));
+        });
+        res
+          .status(200)
+          .end();
+      })
+      .catch((err) => {
+        res
+          .status(500)
+          .send(`Error querying Spanner: ${err}`)
+          .end();
+      });
+}
+
 exports.helloGET = function helloGET (req, res) {
     res.send(`Helloooo ladies`);
 };
